13282a19529ba528c1c372d7f8df739906eabb9b
deltag0 <110630642+deltag0@users.noreply.github.com>
2025-06-08 18:57:04 -0400

    Delete installation/install.js

--- installation/install.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onInstalled.addListener(details => {
-    if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
-        chrome.tabs.create({
-            url: chrome.runtime.getURL("install.html")
-        });
-    }
-    chrome.runtime.sendMesage({ type: "RESET" });
-});
